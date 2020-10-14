489b849010cdf4d96072a3fc71a944ef8655c5e6
Aditya.Priyam <aditya.priyam@ril.com>
2020-10-14 12:17:47 +0530

    set auth details

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const push = require('web-push')
 
+// keys = push.generateVAPIDKeys()
+// console.log(keys)
 const publicKey =
-  'BLOE5991_TKp5L3WHuGUFLmGSM8GOtuaNva7Xdk2tSY68MHlOtTpKOyFvLvDOQIFc4DrpVkazYIKzcSb0XTbqJI'
-const privateKey = 'KibSpclmZ_yPKnv9kmiqtWrHlfK1fd2iZjyR8uFho9A'
+  'BAnfc2MnohFebTMekcqaKja8lW2nsaqXK8rWGM2xUprqHJuo972S5YR9jPOlpzcckOCBIcDPxmWVC4YdgqYTCs0'
+const privateKey = 'HWnjl4ruWzaEJQT2vd9uyDZtzEm1YY4owxNRm6Tmw_o'
 
 let subscription = {
   endpoint:
-    'https://fcm.googleapis.com/fcm/send/cg6IQEA6_Es:APA91bE-mxUBEmYfVdvlklkLKELx1mC72QxafW7BY5nf__mxmqPLmOHbQTzGZC_DTJKNLrFU0qZIeHPjej6Vhcptotk4__RK70IePZkBUnPDwx2YR_6FhRSyFRXuRhMSqRmxXi8OaoJl',
+    'https://fcm.googleapis.com/fcm/send/drCYrfkiWOE:APA91bHttKhdiwd8tkJVySyDB6BLf2lwDrsZjwrk3w5FweVy6Y_GbPaA8qPGUMfSc46LXhYYUrOC-gZETAGO_GfSY-EhkeTEXMsa1R6KHyruYKgr_HNv9bWltbW4w5YidgMJWHlsiamK',
   expirationTime: null,
   keys: {
     p256dh:
-      'BMENJa1ft9qkUV0u2CeJHKj5EcbDVJq37Z63yEiWqmdDC5FwBlyUu7n-OT5y1Wp0yGQ9UwY-fP774iYFPRJbRjg',
-    auth: 'NbFZhyHKhRFEljJ7Ks0DPg'
+      'BGe-80SPDFMRmvjpmwbPsaRuptp_3tYNMOlH6ah5PUHOFvBnR8DbwBXI3G3331mDX-6zAqxOkNODBx-nOs32qHY',
+    auth: '5Hw1MdtFLhJnmCCHCl53Sg'
   }
 }
+
+push.setVapidDetails('mailto:aditya@priyam@ril.com', publicKey, privateKey)
 
 push
   .sendNotification(subscription, 'test message')
