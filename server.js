8059906f279efe0e1f3401a32f3db56089f97265
Aditya.Priyam <aditya.priyam@ril.com>
2020-10-14 11:51:48 +0530

    updated service worker

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const privateKey = 'KibSpclmZ_yPKnv9kmiqtWrHlfK1fd2iZjyR8uFho9A'
 
 let subscription = {
   endpoint:
-    'https://fcm.googleapis.com/fcm/send/eGdq6WAWcbE:APA91bFrf0uw6bTzNyseOjheKJAUIc0-zprVX9cOGJWnub7zGp8X3OwIOTlu5x4tE8QhjGeTfOWK_Q7s9zmY-JZqWTx6pu-AYpw3Nbl7xCNMdVYN_8JzMnHZtr9lgTCNESUGULLGlW8m',
+    'https://fcm.googleapis.com/fcm/send/cg6IQEA6_Es:APA91bE-mxUBEmYfVdvlklkLKELx1mC72QxafW7BY5nf__mxmqPLmOHbQTzGZC_DTJKNLrFU0qZIeHPjej6Vhcptotk4__RK70IePZkBUnPDwx2YR_6FhRSyFRXuRhMSqRmxXi8OaoJl',
   expirationTime: null,
   keys: {
     p256dh:
-      'BJ7ZcaSCDA3nivPuigDw0VsKRtenfUyyIjHIR5LEZlkyHaiATKhQp_XUKyzIFsnAy8y89a2p5hU8PZcEl7GrxfE',
-    auth: 'fMusigcjpO-7ur8OlXyXtw'
+      'BMENJa1ft9qkUV0u2CeJHKj5EcbDVJq37Z63yEiWqmdDC5FwBlyUu7n-OT5y1Wp0yGQ9UwY-fP774iYFPRJbRjg',
+    auth: 'NbFZhyHKhRFEljJ7Ks0DPg'
   }
 }
 
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
-self.addEventListener('push', () => {
-  self.registration.sendNotification('test notification', {})
+self.addEventListener('push', (event) => {
+  let config = {
+    body: 'This is a demo notificaiton',
+    icon: 'images/icon.jpg',
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 2
+    },
+    actions: []
+  }
+  event.waitUntil(
+    self.registration.sendNotification('test notification', config)
+  )
 })
